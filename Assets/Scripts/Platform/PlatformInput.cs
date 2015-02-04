75c6f2a050f348ca18ee2a1224f188fc54c384bf
xec <xec@live.com>
2015-02-04 10:47:47 +0800

    增加手机操作控制

--- Assets/Scripts/Platform/PlatformInput.cs
+++ Assets/Scripts/Platform/PlatformInput.cs
@@ -3,18 +3,38 @@ using System.Collections;
 
 public class PlatformInput : MonoBehaviour
 {
-    public static float GetAxis(string axisName)
+    private static Joystick leftController;
+    private static Joystick rightController;
+    public static void Init()
     {
-        return Input.GetAxis(axisName);
+        leftController = GameObject.FindGameObjectWithTag("LeftJoystick").GetComponent<Joystick>() ;
+        rightController = GameObject.FindGameObjectWithTag("RightJoystick").GetComponent<Joystick>();
+    }
+    public static Vector3 GetMoveDirection() {
+#if UNITY_ANDROID
+       
+        return leftController.position;
+#else
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        return new Vector3(x, 0, z);
+#endif
     }
 
-    public static Vector3 GetTargetPosition()
-    {
+    public static Vector3 GetTargetPosition() {
+#if UNITY_ANDROID
+        return rightController.position;
+#else
         return Input.mousePosition;
+#endif
+
     }
 
-    public static bool GetFire()
-    {
+    public static bool GetFire() {
+#if UNITY_ANDROID
+        return true;
+#else
         return Input.GetButton("Fire1");
+#endif
     }
 }
--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -15,6 +15,8 @@ public class PlayerMove : MonoBehaviour
         floorMask = LayerMask.GetMask("Floor");
         animator = GetComponent<Animator>();
         playerHealth = GetComponent<PlayerHealth>();
+
+        PlatformInput.Init();
     }
 
     // Update is called once per frame
@@ -22,13 +24,14 @@ public class PlayerMove : MonoBehaviour
         if (playerHealth.health <= 0)
             return;
 
-        float x = PlatformInput.GetAxis("Horizontal");
-        float z = PlatformInput.GetAxis("Vertical");
+        //float x = PlatformInput.GetAxis("Horizontal");
+        //float z = PlatformInput.GetAxis("Vertical");
+        Vector3 v = PlatformInput.GetMoveDirection();
 
         // 移动角色
-        Movement(x, z);
+        Movement(v.x, v.y);
         Rotate();
-        UpdateAnimateState(x, z);
+        UpdateAnimateState(v.x, v.y);
     }
 
     void Movement(float x, float z) {
@@ -40,13 +43,17 @@ public class PlayerMove : MonoBehaviour
 
     void Rotate() {
         // 第三人称
-
-
+#if UNITY_ANDROID
+        Vector3 v = PlatformInput.GetTargetPosition();
+        Debug.Log(transform.position);
+        Vector3 playerToMouse = new Vector3(v.x, 0, v.y);
+        Quaternion newRotatation = Quaternion.LookRotation(playerToMouse);
+        rigidbody.MoveRotation(newRotatation);
+#else
         Ray cameraRay = Camera.main.ScreenPointToRay(PlatformInput.GetTargetPosition());
         RaycastHit floorHit;
 
-        if (Physics.Raycast(cameraRay, out floorHit, 100, floorMask))
-        {
+        if (Physics.Raycast(cameraRay, out floorHit, 100, floorMask)) {
             Vector3 playerToMouse = floorHit.point - transform.position;
 
             playerToMouse.y = 0;
@@ -56,6 +63,7 @@ public class PlayerMove : MonoBehaviour
             rigidbody.MoveRotation(newRotatation);
             // transform.rotation = newRotatation;
         }
+#endif
     }
 
     void UpdateAnimateState(float x, float z) {
