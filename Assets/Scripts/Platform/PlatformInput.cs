b29306f92157e8eb4cf69e1a22233fc148acc3eb
xec <xec@live.com>
2015-02-11 17:17:48 +0800

    修复PC的BUG, 增加HDR效果

--- Assets/Scripts/Platform/PlatformInput.cs
+++ Assets/Scripts/Platform/PlatformInput.cs
@@ -5,29 +5,38 @@ public class PlatformInput : MonoBehaviour
 {
     private static Joystick leftController;
     private static Joystick rightController;
-    public static void Init()
-    {
-        leftController = GameObject.FindGameObjectWithTag("LeftJoystick").GetComponent<Joystick>() ;
-        rightController = GameObject.FindGameObjectWithTag("RightJoystick").GetComponent<Joystick>();
+    public static void Init() {
+
+        GameObject obj = GameObject.FindGameObjectWithTag("LeftJoystick");
+        if (obj)
+            leftController = obj.GetComponent<Joystick>();
+
+        obj = GameObject.FindGameObjectWithTag("RightJoystick");
+        if (obj)
+            rightController = obj.GetComponent<Joystick>();
     }
     public static Vector3 GetMoveDirection() {
+        Vector3 v = new Vector3(0, 0, 0);
 #if UNITY_ANDROID
-       
-        return leftController.position;
+        if (leftController) {
+            v = leftController.position;
+        }
 #else
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
-        return new Vector3(x, 0, z);
+        v = new Vector3(x, z, 0);
 #endif
+        return v;
     }
 
     public static Vector3 GetTargetPosition() {
+        Vector3 v = new Vector3(0, 0, 0);
 #if UNITY_ANDROID
-        return rightController.position;
+        v = rightController.position;
 #else
-        return Input.mousePosition;
+        v = Input.mousePosition;
 #endif
-
+        return v;
     }
 
     public static bool GetFire() {
