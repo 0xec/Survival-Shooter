ad2b084c17f6a68cacb76f498d04c6a514b07d3a
xec <xec@live.com>
2015-02-12 18:53:20 +0800

    修复BUG

--- Assets/Scripts/Platform/PlatformInput.cs
+++ Assets/Scripts/Platform/PlatformInput.cs
@@ -7,8 +7,11 @@ public class PlatformInput : MonoBehaviour
     private static Joystick rightController;
     public static void Init()
     {
-        leftController = GameObject.FindGameObjectWithTag("LeftJoystick").GetComponent<Joystick>() ;
-        rightController = GameObject.FindGameObjectWithTag("RightJoystick").GetComponent<Joystick>();
+		GameObject obj = GameObject.FindGameObjectWithTag ("LeftJoystick");
+		if (obj) leftController = obj.GetComponent<Joystick>() ;
+
+		obj = GameObject.FindGameObjectWithTag ("RightJoystick");
+		if (obj) rightController = GameObject.FindGameObjectWithTag("RightJoystick").GetComponent<Joystick>();
     }
     public static Vector3 GetMoveDirection() {
 #if UNITY_ANDROID
@@ -17,7 +20,7 @@ public class PlatformInput : MonoBehaviour
 #else
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
-        return new Vector3(x, 0, z);
+        return new Vector3(x, z, 0);
 #endif
     }
 
